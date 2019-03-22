59afae37486cac1840a789bf2f43a0cba86b7078
stevenzeck <steven.zeck@gmail.com>
2019-03-22 18:08:55 -0500

    Fix code examples not displaying

--- src/UglySamples.ts
+++ src/UglySamples.ts
@@ -3,7 +3,7 @@ export function sample(
   optionKey: string
 ): string | undefined {
   try {
-    return require(`ugly-samples/samples/${languageName}/${optionKey}.txt`);
+    return require(`@unibeautify/ugly-samples/samples/${languageName}/${optionKey}.txt`).default;
   } catch (error) {
     console.error(error);
     return undefined;
